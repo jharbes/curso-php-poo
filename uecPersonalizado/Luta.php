5b75a9c3957316098c90217a79f421758b7f23e9
Jorge Harbes <jharbes@hotmail.com>
2022-08-25 16:16:34 -0300

    uec personalizado

---
+++ uecPersonalizado/Luta.php
@@ -0,0 +1,144 @@
+<?php
+
+require_once 'Lutador.php';
+
+class Luta{
+    private $desafiado;
+    private $desafiante;
+    private $rounds;
+    private $aprovada;
+
+    public function __construct()
+    {
+        $this->setAprovada(false);
+        echo "<p>LUTA CRIADA!";
+    }
+
+    public function marcarLuta($l1,$l2){
+        if ($l1->getCategoria()==$l2->getCategoria() && $l1!=$l2){
+            $this->setAprovada(true);
+            $this->setDesafiado($l1);
+            $this->setDesafiante($l2);
+            echo "<h2>Luta entre ".$l1->getNome()." e ".$l2->getNome()." marcada!</h2>";
+        }else{
+            echo "<h2>Luta não marcada! Favor verificar os dados e tentar novamente!</h2>";
+        }
+    }
+
+    public function lutar(){
+        if ($this->getAprovada()){
+            echo "<hr>";
+            echo "<h2>A LUTA VAI COMEÇAR!</h2>";
+            $this->getDesafiado()->apresentar();
+            $this->getDesafiante()->apresentar();
+            $resultado=rand(0,$this->getDesafiado()->getForca())>=rand(0,$this->getDesafiante()->getForca())?1:2;
+            switch($resultado){
+                case 0:
+                    echo "<h2>EMPATE!</h2>";
+                    $this->getDesafiado()->empatarLuta();
+                    $this->getDesafiante()->empatarLuta();
+                    break;
+                case 1:
+                    echo "<h2>O desafiado {$this->getDesafiado()->getNome()} venceu!</h2>";
+                    $this->getDesafiado()->ganharLuta();
+                    $this->getDesafiante()->perderLuta();
+                    break;
+                case 2:
+                    echo "<h2>O desafiante {$this->getDesafiante()->getNome()} venceu!</h2>";
+                    $this->getDesafiado()->perderLuta();
+                    $this->getDesafiante()->ganharLuta();
+                    break;
+                default:
+                    echo "ERRO!";
+            }
+            echo "<hr>";
+            $this->getDesafiado()->setForca();
+            $this->getDesafiante()->setForca();
+        }
+        else{
+            echo "<h2>Luta não pode ocorrer!</h2>";
+        }
+    }
+
+    /**
+     * Get the value of desafiado
+     */ 
+    public function getDesafiado()
+    {
+        return $this->desafiado;
+    }
+
+    /**
+     * Set the value of desafiado
+     *
+     * @return  self
+     */ 
+    public function setDesafiado($desafiado)
+    {
+        $this->desafiado = $desafiado;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of desafiante
+     */ 
+    public function getDesafiante()
+    {
+        return $this->desafiante;
+    }
+
+    /**
+     * Set the value of desafiante
+     *
+     * @return  self
+     */ 
+    public function setDesafiante($desafiante)
+    {
+        $this->desafiante = $desafiante;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of rounds
+     */ 
+    public function getRounds()
+    {
+        return $this->rounds;
+    }
+
+    /**
+     * Set the value of rounds
+     *
+     * @return  self
+     */ 
+    public function setRounds($rounds)
+    {
+        $this->rounds = $rounds;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of aprovada
+     */ 
+    public function getAprovada()
+    {
+        return $this->aprovada;
+    }
+
+    /**
+     * Set the value of aprovada
+     *
+     * @return  self
+     */ 
+    public function setAprovada($aprovada)
+    {
+        $this->aprovada = $aprovada;
+
+        return $this;
+    }
+}
+
+?>
---
+++ uecPersonalizado/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{
+            border: 1px solid black;
+            width: fit-content;
+            margin: 10px;
+            background-color: darkgrey;
+        }
+        h3.titulo{
+            margin: 0px;
+        }
+        p{
+            width: fit-content;
+            margin: 0px;
+        }
+    </style>    
+</head>
+<body>
+<h1>Curso de PHP-POO</h1>
+<?php
+require_once 'Lutador.php';
+require_once 'Luta.php';
+
+$l[]=new Lutador('Pretty Boy','França',31,1.75,68.9,11,3,1);
+$l[]=new Lutador("Putscript","Brasil",29,1.68,57.8,14,2,3);
+$l[]=new Lutador('Snapshadow',"EUA",35,1.65,80.9,12,2,1);
+$l[]=new Lutador("Dead Code",'Austrália',28,1.93,81.6,13,0,2);
+$l[]=new Lutador("Ufocobol",'Brasil',37,1.70,119.3,5,4,3);
+$l[]=new Lutador("Nerdaard",'EUA',30,1.81,105.7,12,2,4);
+
+$l[0]->apresentar();
+$l[1]->status();
+$l[3]->perderLuta();
+
+$l[3]->apresentar();
+
+$luta[]=new Luta;
+$luta[0]->marcarLuta($l[0],$l[1]);
+$luta[0]->lutar();
+
+$luta[]=new Luta;
+$luta[1]->marcarLuta($l[2],$l[5]);
+
+$luta[1]->marcarLuta($l[2],$l[3]);
+$luta[1]->lutar();
+
+var_dump($l);
+var_dump($luta);
+
+echo $l[0]->getForca()."<br>";
+echo $l[1]->getForca();
+
+?>
+</body>
+</html>
