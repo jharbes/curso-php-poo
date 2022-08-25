63d8375baffa5facbb56dbe548c98fa145c5fee3
Jorge Harbes <jharbes@hotmail.com>
2022-08-25 16:25:12 -0300

    Update index.php

--- uecPersonalizado/index.php
+++ uecPersonalizado/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ultra Emoji Combat</title>
     <style>
         div{
             border: 1px solid black;
