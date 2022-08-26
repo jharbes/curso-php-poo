55c63d79b7e9cd388b3d0e5ffaeb9cc27dd6e4cc
Jorge Harbes <jharbes@hotmail.com>
2022-08-26 10:42:36 -0300

    aula10 inicio

---
+++ aula10-herancaParte01/Pessoa.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Aluno.php';
+require_once 'Professor.php';
+require_once 'Funcionario.php';
+
+
+?>
---
+++ aula10-herancaParte01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Curso de PHP-POO</h1>
+<?php
+require_once 'Pessoa.php';
+require_once 'Aluno.php';
+require_once 'Professor.php';
+require_once 'Funcionario.php';
+
+
+?>
+</body>
+</html>
