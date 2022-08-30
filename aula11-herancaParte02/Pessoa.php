2034fb44d6d4a6d25ea1af06eaf4920c00877ce4
Jorge Harbes <jharbes@hotmail.com>
2022-08-30 10:30:15 -0300

    Update Pessoa.php

--- aula11-herancaParte02/Pessoa.php
+++ aula11-herancaParte02/Pessoa.php
@@ -7,7 +7,74 @@ require_once 'Bolsista.php';
 require_once 'Tecnico.php';
 
 class Pessoa{
+    private $nome;
+    private $idade;
+    private $sexo;
 
+    public function fazerAniv(){
+        echo "<p>Parabéns pelo seu aniversário {$this->getNome()}!</p>";
+        $this->idade++;
+    }
+
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */ 
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idade
+     */ 
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+
+    /**
+     * Set the value of idade
+     *
+     * @return  self
+     */ 
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sexo
+     */ 
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+
+    /**
+     * Set the value of sexo
+     *
+     * @return  self
+     */ 
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+
+        return $this;
+    }
 }
 
 ?>
