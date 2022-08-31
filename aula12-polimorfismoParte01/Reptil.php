821a967c7f58dd058839542904570146f9d1d0d2
Jorge Harbes <jharbes@hotmail.com>
2022-08-31 16:59:10 -0300

    Update Reptil.php

--- aula12-polimorfismoParte01/Reptil.php
+++ aula12-polimorfismoParte01/Reptil.php
@@ -1,7 +1,42 @@
 <?php
 
 class Reptil extends Animal{
+    private $corEscama;
 
+    public function locomover()
+    {
+        echo "<p>Corrida de réptil!</p>";
+    }
+
+    public function alimentar()
+    {
+        echo "<p>Alimentação de répteis!</p>";
+    }
+
+    public function emitirSom()
+    {
+        echo "<p>Som de Réptil!</p>";
+    }
+
+    /**
+     * Get the value of corEscama
+     */ 
+    public function getCorEscama()
+    {
+        return $this->corEscama;
+    }
+
+    /**
+     * Set the value of corEscama
+     *
+     * @return  self
+     */ 
+    public function setCorEscama($corEscama)
+    {
+        $this->corEscama = $corEscama;
+
+        return $this;
+    }
 }
 
 ?>
