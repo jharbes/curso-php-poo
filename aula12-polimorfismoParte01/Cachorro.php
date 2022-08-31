ecc0275c4aacfd3212e421c9a0c570afa38533bc
Jorge Harbes <jharbes@hotmail.com>
2022-08-31 17:10:39 -0300

    Update Cachorro.php

--- aula12-polimorfismoParte01/Cachorro.php
+++ aula12-polimorfismoParte01/Cachorro.php
@@ -1,7 +1,13 @@
 <?php
 
 class Cachorro extends Mamifero{
+    public function enterrarOsso(){
+        echo "<p>Cachorro enterrou osso!</p>";
+    }
 
+    public function abanarRabo(){
+        echo "<p>Cachorro abanando rabo!</p>";
+    }
 }
 
 ?>
