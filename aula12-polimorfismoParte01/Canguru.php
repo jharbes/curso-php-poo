98f9217775d4bff9d765b89fb1ea6c19e01e2eeb
Jorge Harbes <jharbes@hotmail.com>
2022-08-31 17:09:19 -0300

    Update Canguru.php

--- aula12-polimorfismoParte01/Canguru.php
+++ aula12-polimorfismoParte01/Canguru.php
@@ -1,7 +1,14 @@
 <?php
 
 class Canguru extends Mamifero{
+    public function usarBolsa(){
+        echo "<p>Usando bolsa de canguru!</p>";
+    }
 
+    public function locomover()
+    {
+        echo "<p>Saltando como um canguru!</p>";
+    }
 }
 
 ?>
