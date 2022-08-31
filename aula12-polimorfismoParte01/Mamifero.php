ed75a1d927c7f9884bd11e63e321f90c9fbbba73
Jorge Harbes <jharbes@hotmail.com>
2022-08-31 16:55:29 -0300

    Update Mamifero.php

--- aula12-polimorfismoParte01/Mamifero.php
+++ aula12-polimorfismoParte01/Mamifero.php
@@ -1,7 +1,42 @@
 <?php
 
 class Mamifero extends Animal{
+    private $corPelo;
 
+    public function locomover()
+    {
+        echo "<p>Mamífero correndo!</p>";
+    }
+
+    public function alimentar()
+    {
+        echo "<p>Mamífero mamando!</p>";
+    }
+
+    public function emitirSom()
+    {
+        echo "<p>Som de mamífero</p>";
+    }
+
+    /**
+     * Get the value of corPelo
+     */ 
+    public function getCorPelo()
+    {
+        return $this->corPelo;
+    }
+
+    /**
+     * Set the value of corPelo
+     *
+     * @return  self
+     */ 
+    public function setCorPelo($corPelo)
+    {
+        $this->corPelo = $corPelo;
+
+        return $this;
+    }
 }
 
 ?>
