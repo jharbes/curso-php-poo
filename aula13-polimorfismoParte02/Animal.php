5151fcd0a2974ed608b8b80b46042fe0adc1e3fe
Jorge Harbes <jharbes@hotmail.com>
2022-09-01 09:33:03 -0300

    criacao arquivos

---
+++ aula13-polimorfismoParte02/Animal.php
@@ -0,0 +1,11 @@
+<?php
+
+abstract class Animal{
+    protected $peso;
+    protected $idade;
+    protected $membros;
+
+    public abstract function emitirSom();
+}
+
+?>
---
+++ aula13-polimorfismoParte02/Cachorro.php
@@ -0,0 +1,11 @@
+<?php
+
+class Cachorro extends Lobo{
+
+    public function emitirSom()
+    {
+        echo "<p>Au! Au! Au!</p>";
+    }
+}
+
+?>
